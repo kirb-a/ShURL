4816eb87cad9df2667f2ea006f567e5684be311f
Bakir Pasalic <bakirpasalic@gmail.com>
2024-07-19 00:11:06 +0200

    ShUrl

--- python.py
+++ python.py
@@ -1,11 +1,11 @@
 import requests
 
 def shorter_link(full, link_n):
-    API_KEY = '2141d1a732e7b0beee169982ac4cda58d417e'
-    BASE_URL = 'https://cutt.ly/api/api.php'
+    API = '2141d1a732e7b0beee169982ac4cda58d417e'
+    BASE = 'https://cutt.ly/api/api.php'
 
-    pay = {'key': API_KEY, 'short': full, 'name': link_n}
-    response = requests.get(BASE_URL, params=pay)
+    pay = {'key': API, 'short': full, 'name': link_n}
+    response = requests.get(BASE, params=pay)
     data = response.json()
 
     try:
@@ -18,7 +18,7 @@ def shorter_link(full, link_n):
         status = data['url']['status']
         print('Error Status:', status)
 
-link = input('Enter a Link: ')
-name = input('Give your link a name: ')
+link = input('Enter your link : ')
+name = input('Name your link : ')
 
 shorter_link(link, name)
